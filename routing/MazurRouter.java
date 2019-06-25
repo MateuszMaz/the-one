21d3da10db4c06e77625f35d0333b451447ca1fa
Mazur <mtma@MobicaPL.local>
2019-06-25 15:44:52 +0200

    Add packet classes for AODV

--- routing/MazurRouter.java
+++ routing/MazurRouter.java
@@ -5,12 +5,36 @@ import core.Settings;
 
 public class MazurRouter extends ActiveRouter {
 
+	private class RREQ {
+		public String sourceAddress;
+		public int sourceSequence; //timestamp wysłania RREQ
+		public int broadcastId;
+		public String destinationAddress;
+		public int destinationSequence; //0?
+		public int hopCount;  
+	}
+
+	private class RREP {
+		public String sourceAddress;
+		public String destinationAddress;
+		public int sourceSequence;
+		public int hopCount;
+		public int lifeTime;
+	}
+
+	List<RREQ> RREQPackets;
+	List<RREP> RREPPackets;
+
 	protected MazurRouter(ActiveRouter r) {
 		super(r);
+		RREPPackets = new ArrayList();
+		RREQPackets = new ArrayList();
 	}
 
 	public MazurRouter(Settings s) {
 		super(s);
+		RREPPackets = new ArrayList();
+		RREQPackets = new ArrayList();
 	}
 
 	@Override
@@ -20,7 +44,6 @@ public class MazurRouter extends ActiveRouter {
 
 	@Override
 	public void changedConnection(Connection con) {
-		super.changedConnection(con);
-		
+		super.changedConnection(con);	
 	}
 }
